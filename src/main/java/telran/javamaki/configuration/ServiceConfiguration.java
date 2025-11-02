704a53e569816ef514519d8383077c49a73c931f
StrelArt <streltsyn.artem@gmail.com>
2025-11-02 23:11:35 +0200

    Built with Ira's credentials

--- src/main/java/telran/javamaki/configuration/ServiceConfiguration.java
+++ src/main/java/telran/javamaki/configuration/ServiceConfiguration.java
@@ -6,8 +6,6 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableMongoAuditing
